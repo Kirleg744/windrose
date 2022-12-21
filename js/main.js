d15dd55d351dc79763e18bdbcf9dc1977e264bf7
Kirleg744 <lyogky.kirill@gmail.com>
2022-12-21 23:06:44 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// const swiper = new Swiper('.about-container', {
+//         speed: 500,
+//         loop: true,
+//         spaceBetween: 10,
+//         slidesPerView: 1,
+//         navigation: {
+//             nextEl: '.about__slide-next',
+//             prevEl: '.about__slide-prev',
+//         },
+// });
